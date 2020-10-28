1d79f3aa1691b3f8afd3c15804d58f9ed0532b89
Bokie Tarathep <tarathec@ais.co.th>
2020-10-28 10:54:17 +0700

    refix name

--- RobotKafkaLibrary/Consumer.py
+++ RobotKafkaLibrary/Consumer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
 
-def Subcribe(broker,topic,timeout=3000):
+def Subscribe(broker,topic,timeout=3000):
     consumer = KafkaConsumer(bootstrap_servers=broker,auto_offset_reset='earliest',enable_auto_commit=True,
                          auto_commit_interval_ms=1000, group_id='robot-test',consumer_timeout_ms=timeout)
     consumer.subscribe([topic])
