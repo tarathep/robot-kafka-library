01f097e8d788cf5669133bfcf57ab131317dd27a
Bokie Tarathep <tarathec@ais.co.th>
2020-10-28 10:55:14 +0700

    renew ver 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="robot-kafka-library",
-    version="0.0.3",
+    version="0.0.4",
     author="tarathep",
     author_email="bokie.tarathep@gmail.com",
     description="robotframework exetension lib for test kafka",
